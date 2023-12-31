062ecefe0737d56a1675b8381974cdd5cbf42be8
asdess23 <155285066+asdess23@users.noreply.github.com>
2023-12-31 14:43:04 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJpOI79kKFWf08v3b6AoTf85HZRKaCIuI",
+  authDomain: "vuhyfy-1b959.firebaseapp.com",
+  databaseURL: "https://vuhyfy-1b959-default-rtdb.firebaseio.com",
+  projectId: "vuhyfy-1b959",
+  storageBucket: "vuhyfy-1b959.appspot.com",
+  messagingSenderId: "175875871183",
+  appId: "1:175875871183:web:76ac8fe11059228e6782ba"
+};
